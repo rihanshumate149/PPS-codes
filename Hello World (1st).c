fd549399614dbd595b50e23c2b713ed1bd59c1b5
Rihanshu mate <rihanshumate149@gmail.com>
2026-07-09 17:09:01 +0530

    Update Hello World (1st).c

--- Hello World (1st).c
+++ Hello World (1st).c
@@ -1,10 +1,9 @@
-// Name:-Rihanshu U. Mate.
-// PRN No:-2503033111378UD030
 #include<stdio.h>
  int main()
   {
  	printf("Hello World");
  	return 0;
    } 
-  
+// Name:-Rihanshu U. Mate.
+// PRN No:-2503033111378UD030  
 
